18f45826d3afeac98bd082c9573222bb865d416e
RavindraMedhat <ravindrasinhmedhat@gmail.com>
2023-09-19 11:19:10 +0530

    add RoomCategoryTB

--- Models/HotelTB.cs
+++ Models/HotelTB.cs
@@ -14,6 +14,7 @@ namespace Hotel_Management.Models
         [Required]
         [MaxLength(50,ErrorMessage ="Name can not more than 50 character")]
         public string Hotel_Name { get; set; }
+
         [Required]
         [MaxLength(300, ErrorMessage = "Description can not more than 300 character")]
         public string Hotel_Description { get; set; }
